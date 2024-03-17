77a135249f8d45169775e192a8e945f9425950f5
= <omerfarukkpala@gmail.com>
2024-03-17 20:30:58 +0300

    ILogger<HomeController> logger

--- Environment.Web/Controllers/HomeController.cs
+++ Environment.Web/Controllers/HomeController.cs
@@ -14,6 +14,7 @@ namespace Environment.Web.Controllers
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
+
         {
             _logger = logger;
         }
